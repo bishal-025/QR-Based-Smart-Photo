5a82f1b5c50b6f3abfd3b24c50ad7ee8d354c628
bishalawasthi <bishalawasthi2019@gmail.com>
2026-02-17 12:09:12 -0600

    Add Auth

---
+++ Login-Module/src/test/java/com/qr/photo/login/LoginModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qr.photo.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
